ed43166e411495b017dca65ce9b28122bc4e36f7
KM4HS <152136532+KM4HS@users.noreply.github.com>
2024-10-30 09:49:23 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.08 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,18 +1,16 @@
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
-        int lcm = 0;
-        int gcf = 0;
         int min = (n > m)? m:n;
         int max = (n > m)? n:m;
         
-        // lcm
+        // gcd
         for(int i=1; i<=min; i++){
             if(min % i == 0 && max % i == 0){
                 answer[0] = i;
             }
         }
-        // gcf
+        // lcm
         for(int i=1; i<=min; i++){
             if((max * i) % min == 0){
                 answer[1] = max * i;
