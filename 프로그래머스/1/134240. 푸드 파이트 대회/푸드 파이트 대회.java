1746d7ed38372d0798b68a0b845811c786e69054
KM4HS <152136532+KM4HS@users.noreply.github.com>
2024-11-11 10:16:25 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 21.87 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,11 +1,9 @@
 class Solution {
     public String solution(int[] food) {
         StringBuilder sb = new StringBuilder();
-        int foodType = 0;
         for(int i=1; i<food.length; i++) {
-            foodType++;
             for(int j=0; j<food[i]/2; j++) {
-                sb.append(foodType);
+                sb.append(i);
             }
         }
         String answer = sb.toString() + "0" + sb.reverse();
