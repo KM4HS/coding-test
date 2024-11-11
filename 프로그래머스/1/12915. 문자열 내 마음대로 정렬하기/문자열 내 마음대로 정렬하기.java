4c65ecf623bf0568ab6fc0b7f71a526f5f5d95a7
KM4HS <152136532+KM4HS@users.noreply.github.com>
2024-11-11 09:58:12 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.40 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,25 +1,17 @@
-import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        Map<Character, List<String>> map = new HashMap<>();
-        List<String> result = new ArrayList<>();
-        for(String s : strings) {
-            if(map.get(s.charAt(n)) == null) {
-                map.put(s.charAt(n), new ArrayList<>());
-            }
-            map.get(s.charAt(n)).add(s);
+        for(int i=0; i<strings.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(strings[i].charAt(n)).append(strings[i]);
+            strings[i] = sb.toString();
         }
-        map.keySet().stream()
-                .sorted()
-                .forEach(a -> {
-                    map.get(a).stream().sorted()
-                            .forEach(result::add);
-                });
-
-        return result.toArray(String[]::new);
+        Arrays.sort(strings);
+        String[] answer = new String[strings.length];
+        for(int i=0; i<strings.length; i++) {
+            answer[i] = strings[i].substring(1);
+        }
+        return answer;
     }
 }
