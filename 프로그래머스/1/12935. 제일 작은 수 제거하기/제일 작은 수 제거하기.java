bcdc3f597799a3658a94111337493564fa16e142
KM4HS <152136532+KM4HS@users.noreply.github.com>
2024-11-13 14:25:27 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.89 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,12 +1,30 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] arr) {
-        if(arr.length <= 1){
-            return new int[]{-1};
+        int[] answer;
+        int min = arr[0];
+        
+        if(arr.length == 1) {
+            answer = new int[]{-1};
+            return answer;
         } else {
-            int min = Arrays.stream(arr).min().getAsInt();
-            return Arrays.stream(arr).filter(i -> i!=min).toArray();
+            answer = new int[arr.length - 1];
         }
+
+        for(int i=0; i<arr.length; i++) {
+            if(min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        
+        int index = 0;
+        
+        for(int n : arr) {
+            if(n == min) {
+                continue;
+            }
+            answer[index++] = n;
+        }
+
+        return answer;
     }
 }
