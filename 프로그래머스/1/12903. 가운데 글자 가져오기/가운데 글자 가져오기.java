101e14523517f8b8426313f7cf655b87840a2e4f
KM4HS <152136532+KM4HS@users.noreply.github.com>
2024-11-14 09:15:10 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,12 +1,11 @@
 class Solution {
     public String solution(String s) {
-        char[] chArr = s.toCharArray();
         StringBuilder sb = new StringBuilder();
-        if(s.length()%2 == 0){
-            sb.append(chArr[s.length()/2-1]);
-            sb.append(chArr[s.length()/2]);
-        }else{
-            sb.append(chArr[s.length()/2]);
+        if(s.length()%2 == 0) {
+            sb.append(s.charAt(s.length()/2 - 1))
+                .append(s.charAt(s.length()/2));
+        } else {
+            sb.append(s.charAt(s.length()/2));
         }
         return sb.toString();
     }
